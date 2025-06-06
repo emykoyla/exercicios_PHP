70c7bb5c1132bfa4095f76b5720c535ebd4b8ca1
Emy <150801649+emykoyla@users.noreply.github.com>
2025-06-06 10:05:34 -0300

    Add files via upload

---
+++ emillyPHP/Formulários/PHP/formulario03.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>LALALALLA</h1>
+
+    <?php
+        $nro = $_GET['nro'];
+
+        $calcular = $nro * 7;
+        echo"A multiplicação por 7 é: $calcular.";
+        ?>
+</body>
+</html>
